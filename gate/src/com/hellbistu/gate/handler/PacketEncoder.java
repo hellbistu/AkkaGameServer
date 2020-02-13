959458ca8635fa3e892b05a1a19e90b393e1ab65
yangzhenyu <1071343872@qq.com>
2020-02-13 16:04:57 +0800

    工程结构提交

---
+++ gate/src/com/hellbistu/gate/handler/PacketEncoder.java
@@ -0,0 +1,15 @@
+package com.hellbistu.gate.handler;
+
+import com.hellbistu.gate.Packet;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class PacketEncoder extends MessageToByteEncoder<Packet> {
+
+    protected void encode(ChannelHandlerContext channelHandlerContext, Packet packet, ByteBuf out) {
+        out.writeShortLE(packet.getPtype());
+        out.writeIntLE(packet.getLength());
+        out.writeBytes(packet.getPdata());
+    }
+}
